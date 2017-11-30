4325d826a657a4ef8b38e41f81c36e1d9b432a61
Felix Neumeyer <felix@neumeyer.it>
2017-11-30 19:24:04 +0100

    Added DebugInput.cs which implements IShareInput and is emulating a Share Device with Keyboard Input.

--- Assets/Scripts/ShareActions/ShareAction.cs
+++ Assets/Scripts/ShareActions/ShareAction.cs
@@ -52,7 +52,7 @@ public abstract class ShareAction : MonoBehaviour {
     /// You can do all the setup you need for the action here.
     /// </summary>
     public virtual void EnterAction() {
-        Debug.Log("Exiting Action " + GetType());
+        Debug.Log("Enter Action " + GetType());
         _active = true;
     }
 
@@ -62,6 +62,7 @@ public abstract class ShareAction : MonoBehaviour {
     /// </summary>
     public virtual void ExitAction()
     {
+        Debug.Log("Exit Action " + GetType());
         _active = false;
     }
 	
---
+++ Assets/Scripts/ShareDevice/DebugInput.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugInput : MonoBehaviour, IShareInput {
+
+    private float _force = 0;
+
+    private Quaternion _rotation = Quaternion.identity;
+
+    private bool _isPickedUp = false;
+
+    public Vector3 GetAccelerationRaw()
+    {
+        return Vector3.zero;
+    }
+
+    public float GetForce()
+    {
+        return _force;
+    }
+
+    public Quaternion GetRotation()
+    {
+        return _rotation;
+    }
+
+    public float GetTiltAngle()
+    {
+        return Quaternion.Angle(Quaternion.identity, _rotation);
+    }
+
+    public bool IsPickedUp()
+    {
+        return _isPickedUp;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CheckForce();
+        CheckRotation();
+	}
+
+    void CheckForce()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            _force = Mathf.Clamp(_force + Time.deltaTime * 500, -5, 5000);
+        }
+        else
+        {
+            _force = Mathf.Clamp(_force - Time.deltaTime * 500, -5, 5000);
+        }
+    }
+
+    void CheckRotation()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        Quaternion.RotateTowards(_rotation, Quaternion.Euler(0, 0, (horizontal / Mathf.Abs(horizontal)) * 70), Mathf.Abs(horizontal) * Time.deltaTime);
+    }
+
+    void CheckPickUp()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            _isPickedUp = !_isPickedUp;
+        }
+    }
+}
