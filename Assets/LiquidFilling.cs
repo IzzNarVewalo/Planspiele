06df0995d5367011d1582602b9a9992933af7b41
Felix Neumeyer <felix@neumeyer.it>
2018-01-22 03:55:45 +0100

    Added Shaders/LiquidFilling.shader to fill objects that have a "inner wall".

---
+++ Assets/LiquidFilling.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class LiquidFilling : MonoBehaviour {
+
+    Renderer _renderer;
+
+    [Range(0, 1)][SerializeField]
+    private float FillLevel;
+
+    private float _worldLiquidLimit;
+
+    // Use this for initialization
+    void Start () {
+        _renderer = GetComponent<Renderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float newLimit = _renderer.bounds.min.y + (_renderer.bounds.max.y - _renderer.bounds.min.y) * FillLevel;
+        if(_worldLiquidLimit != newLimit)
+        {
+            _worldLiquidLimit = newLimit;
+            _renderer.sharedMaterial.SetFloat("_LiquidHeight", _worldLiquidLimit);
+        }
+        
+    }
+
+    public void SetFillLevel(float level)
+    {
+        FillLevel = Mathf.Clamp(level, 0, 1);
+    }
+
+    private void OnValidate()
+    {
+        SetFillLevel(FillLevel);
+    }
+    
+}
